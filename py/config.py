4a1b54a717ea783206267b1a9051a403d83a9842
nathan <nathan@negativenull.net>
2014-10-01 12:58:51 -0600

    database class

---
+++ py/config.py
@@ -0,0 +1,13 @@
+__author__ = 'Nathan Meadows'
+
+
+class Config:
+
+    dbname = 'cms'
+    dbhost = 'localhost'
+    dbport = 5432
+    dbusername = 'nathan'
+    dbpassord = ''
+
+    def __init_(self):
+        pass
--- py/db.py
+++ py/db.py
@@ -1,7 +1,60 @@
-__author__ = 'nathan'
-
+__author__ = 'Nathan Meadows'
 import psycopg2
+from py.config import Config
+
+
 class DB:
 
+    conn = None
+    cur = None
+
     def __init__(self):
-        pass
+        conn = psycopg2.connect("dbname=%s user=%s password=%s port=%d"
+                                % (Config.dbname,
+                                   Config.dbusername,
+                                   Config.dbpassword,
+                                   Config.dbport
+                                   )
+                                )
+
+    def query(self, query, returnall=False):
+        self.cur = self.conn.execute(query)
+        if returnall:
+            return self.cur.fetchall()
+
+    def fetchall(self):
+        return self.cur.fetchall()
+
+    def fetchone(self):
+        return self.cur.fetchone()
+
+    def numrows(self):
+        return self.cur.rowcount()
+
+'''
+# Connect to an existing database
+>>> conn = psycopg2.connect("dbname=test user=postgres")
+
+# Open a cursor to perform database operations
+>>> cur = conn.cursor()
+
+# Execute a command: this creates a new table
+>>> cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+
+# Pass data to fill a query placeholders and let Psycopg perform
+# the correct conversion (no more SQL injections!)
+>>> cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",
+...      (100, "abc'def"))
+
+# Query the database and obtain data as Python objects
+>>> cur.execute("SELECT * FROM test;")
+>>> cur.fetchone()
+(1, 100, "abc'def")
+
+# Make the changes to the database persistent
+>>> conn.commit()
+
+# Close communication with the database
+>>> cur.close()
+>>> conn.close()
+'''
