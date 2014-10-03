597918d0c0f4077f3b6af6a1b3bef562854493fb
Nathan Meadows <nathan.meadows@blackboard.com>
2014-10-02 20:28:59 -0600

    templates

---
+++ controllers/page.py
@@ -0,0 +1,24 @@
+__author__ = 'Nathan Meadows'
+
+from cgi import escape
+from py.tag import Tag
+from py.db import DB
+from py.urls import Urls
+
+
+class Page(Urls):
+
+    @staticmethod
+    def page(environ, start_response):
+
+        args = environ['myapp.url_args']
+        if args:
+            pageid = escape(args[0])
+            db = DB()
+            page = db.query("select * from pages where id=%s" % pageid, returnone=True)
+
+        else:
+            return Urls.not_found
+
+        return [Urls.wrapTemplate(page['content'])]
+
--- py/router.py
+++ py/router.py
@@ -1,10 +1,11 @@
 from py.urls import Urls
+from controllers.page import Page
 
 #map urls to function
 router = [
             (r'^$', Urls.index),
             (r'hello/?$', Urls.hello),
             (r'hello/(.+)$', Urls.hello),
-            (r'page/?$', Urls.page),
-            (r'page/(.+)$', Urls.page)
+            (r'page/?$', Page.page),
+            (r'page/(.+)$', Page.page)
         ]
